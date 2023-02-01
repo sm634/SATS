cf6fbb85882c1dfa2ad0b8eb1226ec6ecb083d48
Mukhia <safal.mukhia@capgemini.com>
2023-02-01 17:54:42 +0000

    inital pdf summarizer demo commit
    Signed-off-by: Mukhia <safal.mukhia@capgemini.com>

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
 import os
-
-import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import redirect, render_template, request, url_for
+from forms import UploadFile
 from prompts import summarize_text
+import openai
+from flask import Flask
+from werkzeug.utils import secure_filename
+import pdf2image.exceptions
+from pdf2image import convert_from_path
+
+import pytesseract
+from PIL import Image
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'  # required for crf secret token of the form.
+app.config['UPLOAD_FOLDER'] = 'static/files'
+app.config['IMAGE_FOLDER'] = app.config['UPLOAD_FOLDER'] + '/converted_images/'
+app.config['SUMMARIES_FOLDER'] = app.config['UPLOAD_FOLDER'] + '/summaries/'
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\samukhia\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+
+def get_file_extension(filename):
+    return filename.rsplit('.', 1)[1].lower()
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/", methods=('GET', 'POST'))
@@ -17,6 +41,47 @@ def index():
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
+
+
+@app.route("/upload", methods=('GET', 'POST'))
+def upload():
+    form = UploadFile()
+    if form.validate_on_submit():
+        filename = request.files['file'].filename
+
+        # only allow files with the approved extensions to be uploaded.
+        if allowed_file(filename):
+            file = form.file.data  # first grab the file
+            file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                   app.config['UPLOAD_FOLDER'],
+                                   secure_filename(file.filename)))
+
+            if get_file_extension(filename) == 'pdf':
+                # remove extension of filename
+                filename = filename.replace(r' ', '_')
+                # convert pdf to image
+                pdf_pages = convert_from_path(app.config['UPLOAD_FOLDER'] + f'/{filename}')
+                # save image from pdf.
+                filename = filename.replace('.pdf', '')
+                image_path = app.config['IMAGE_FOLDER'] + f'{filename}'
+                for page in pdf_pages:
+                    page.save(image_path, "JPEG")
+
+                # use ocr to extract image from text.
+                text = pytesseract.image_to_string(Image.open(image_path))
+                response = summarize_text(text)
+
+                summary_txt_path = app.config['SUMMARIES_FOLDER'] + f'{filename}.txt'
+                with open(summary_txt_path, 'w') as f:
+                    f.write(response)
+                    f.close()
+
+                return render_template("upload.html", form=form, result=response)
+
+        else:
+            redirect(url_for("upload"))
+
+    return render_template('upload.html', form=form)
 
 
 if __name__ == '__main__':
--- extraction_layer/pdf_to_image.py
+++ extraction_layer/pdf_to_image.py
@@ -4,7 +4,7 @@ import pdf2image.exceptions
 from pdf2image import convert_from_path
 
 
-def get_pdf_file_paths(dir_path='data/input/'):
+def get_pdf_file_paths(dir_path='data/input/pdfs/'):
     """
     Finds all pdf files in a directory and stores them in a list along with their relative path.
     :param dir_path: the directory to search for the pdf files.
@@ -16,7 +16,7 @@ def get_pdf_file_paths(dir_path='data/input/'):
     return pdf_file_paths
 
 
-def get_pdf_names(dir_path='data/input/'):
+def get_pdf_names(dir_path='data/input/pdfs/'):
     """
     Finds all pdf file names in a directory and stores them in a list along with their relative path.
     :param dir_path: the directory to search for the pdf files.
@@ -28,28 +28,33 @@ def get_pdf_names(dir_path='data/input/'):
     return pdf_file_names
 
 
-def save_images(pages, doc_name, output_dir='data/output/'):
+def save_images(pages, doc_name, output_dir='data/input/images/'):
     """
     Take in pdf to image converted pages and save them in a designated directory.
-    :param pages: the input pdf pages converted to from_images to be saved
+    :param page: the input pdf pages converted to from_images to be saved
     :param doc_name: name of the document to save as.
     :param output_dir: the directory to save the output from_images.
     :return: output from_images
     """
     for i, page in enumerate(pages):
-        image_name = f"{doc_name}_page_{i+1}.jpg"
+        image_name = f"{doc_name}_page_{i + 1}.jpg"
         output_img = output_dir + image_name
         page.save(output_img, "JPEG")
 
 
-pdfs_paths = get_pdf_file_paths()
-pdf_names = get_pdf_names()
+def save_images_from_file():
+    pdfs_paths = get_pdf_file_paths()
+    pdf_names = get_pdf_names()
+    for j, pdf in enumerate(pdfs_paths):
+        try:
+            pages = convert_from_path(pdf)
+            print(pages)
+            breakpoint()
+            file_name = pdf_names[j]
+            save_images(pages=pages, doc_name=file_name)
+            print(f"image {file_name} saved")
+        except pdf2image.exceptions.PDFPageCountError:
+            pass
 
-for j, pdf in enumerate(pdfs_paths):
-    try:
-        pages = convert_from_path(pdf)
-        file_name = pdf_names[j]
-        save_images(pages=pages, doc_name=file_name)
-    except pdf2image.exceptions.PDFPageCountError:
-        pass
 
+save_images_from_file()
